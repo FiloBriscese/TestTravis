4ebd2bf1ca9a650e469e8f071bd4aa9dcdb1f5d8
Filippo Maria Briscese <24792631+FiloBriscese@users.noreply.github.com>
2020-10-22 16:10:12 +0200

    commented the line without ';'

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// print something
-		//System.out.println("Hello World!");
+		System.out.println("Hello World!");
 		
 		// bug intended to test travis
-		System.out.println("Hello bug!")
+		//System.out.println("Hello bug!")	//travis test passed even without ;
 	}
 
 }
