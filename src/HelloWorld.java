0cdd91251e0209308cf956fd0ba221d40cdef34c
Filippo Maria Briscese <24792631+FiloBriscese@users.noreply.github.com>
2020-10-23 09:40:41 +0200

    Code compile on Eclipse, let's see on Travis. Previously code with bug: Passed; code without bug: Failed.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,8 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// print something
 		System.out.println("Hello World!");
-		
-		// bug intended to test travis
-		//System.out.println("Hello bug!")	//travis test passed even without ;
 	}
-
+	
 }
